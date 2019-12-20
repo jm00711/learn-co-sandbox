745e43e4dd53b4ee1cc157a077dae9469ac2e23f
Joseph Adamick <joeadamick97@gmail.com>
2019-12-20 17:06:04 +0000

    Automatically backed up by Learn

---
+++ arguments.rb
@@ -0,0 +1,11 @@
+def greeting_programmer(name, language = "computer")
+  puts "Hello,  #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Steven")
+
+def greeting(name = 'neighbor')
+  puts "Hello, #{name}!"
+end
+
+#greeting("Steven")
---
+++ while-do.rb
@@ -0,0 +1,31 @@
+#count = 0 
+#while count < 3 do
+ # puts "I am the #{count}, I love to count!"
+ # count -= 1 
+#end 
+
+#magic_exit_number = 7
+#count = 0 
+#while count < 10 && count != magic_exit_number do
+ # puts "I am the #{count}, I love to count!"
+  #count = count + 1 
+#end 
+
+#count = 0  
+#n = 3  
+#while true do 
+#  break if count >= n 
+#  puts " I ran."
+ # count += 1 
+#end
+
+#3.times do 
+ # puts "I ran."
+#end
+
+counter = 0 
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1 
+end 
+  
